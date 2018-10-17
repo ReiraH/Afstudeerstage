9e888cf3005160710fd01439da26fb32499e4d8a
Reira <rheahau@gmail.com>
2018-10-17 18:33:53 +0200

    esp32 werkt met pulse sensor

--- test/PulseBreathGSRsensor/src/main.cpp
+++ test/PulseBreathGSRsensor/src/main.cpp
@@ -1,13 +1,18 @@
 #include <Arduino.h>
 
+#define ANALOG_PIN_1 0
+int analog_value = 0;
+
 void setup() {
-  Serial.begin(9600);
-  pinMode(0, OUTPUT);
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("ESP32 Analog IN Test");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(analogRead(0));
-  delay(10);
+  analog_value = analogRead(ANALOG_PIN_1);
+  Serial.println(analog_value);
+  delay(500);
   
 }
