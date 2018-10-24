746f5e64329bcf9abde20191f1b01cd6e653c852
Rhea Hau <rheahau@gmail.com>
2018-10-24 10:26:41 +0200

    extra hoofdstukken toegevoegd aan scriptie

--- prog/PulseBreathGSRsensor/src/main.cpp
+++ prog/PulseBreathGSRsensor/src/main.cpp
@@ -1,92 +1,78 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+
+  You can use this sketch as a debug tool that prints all incoming values
+  sent by a widget connected to a Virtual Pin 1 in the Blynk App.
+
+  App project setup:
+    Slider widget (0...100) on V1
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
 #include <Arduino.h>
-#include <ArduinoJson.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "76c0fbc33bb342b4975ccef95b8d2192";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "reirafoen.";
+char pass[] = "uqiux8xnn8us0";
 
 #define ANALOG_PIN_1 0
 int analog_value = 0;
 
-/*void setup() {
-  Serial.begin(115200);
-  delay(1000);
-  Serial.println("ESP32 Analog IN Test");
+// This function will be called every time Slider Widget
+// in Blynk app writes values to the Virtual Pin 1
+BLYNK_READ(ANALOG_PIN_1)
+{
+  analog_value = analogRead(ANALOG_PIN_1);
+  Blynk.virtualWrite(ANALOG_PIN_1, millis() / 1000);
+  //int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+  // You can also use:
+  // String i = param.asStr();
+  // double d = param.asDouble();
+  Serial.print("V1 Slider value is: ");
+  Serial.println(analog_value);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  analog_value = analogRead(ANALOG_PIN_1);
-  Serial.println(analog_value);
-  delay(500);
-  
-}*/
-
-// ArduinoJson - arduinojson.org
-// Copyright Benoit Blanchon 2014-2018
-// MIT License
-
-
-
-void setup() {
-  // Initialize Serial port
+void setup()
+{
+  // Debug console
   Serial.begin(115200);
-  //while (!Serial) continue;
+  pinMode(ANALOG_PIN_1,INPUT);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
 }
 
-void loop() {
-  // not used in this example
-
-  // Memory pool for JSON object tree.
-  //
-  // Inside the brackets, 200 is the size of the pool in bytes.
-  // Don't forget to change this value to match your JSON document.
-  // Use arduinojson.org/assistant to compute the capacity.
-  StaticJsonBuffer<200> jsonBuffer;
-
-  // StaticJsonBuffer allocates memory on the stack, it can be
-  // replaced by DynamicJsonBuffer which allocates in the heap.
-  //
-  // DynamicJsonBuffer  jsonBuffer(200);
-
-  // Create the root of the object tree.
-  //
-  // It's a reference to the JsonObject, the actual bytes are inside the
-  // JsonBuffer with all the other nodes of the object tree.
-  // Memory is freed when jsonBuffer goes out of scope.
-  JsonObject& root = jsonBuffer.createObject();
-  
-  // Add values in the object
-  //
-  // Most of the time, you can rely on the implicit casts.
-  // In other case, you can do root.set<long>("time", 1351824120);
-  analog_value = analogRead(ANALOG_PIN_1);
-  root["sensor"] = "pulse sensor";
-
-  // Add a nested array.
-  //
-  // It's also possible to create the array separately and add it to the
-  // JsonObject but it's less efficient.
-  JsonArray& data = root.createNestedArray("data");
-  data.add(analog_value);
-  //data.add(0022);
-
-  root.printTo(Serial);
-  // This prints:
-  // {"sensor":"gps","time":1351824120,"data":[48.756080,2.302038]}
-
-  Serial.println();
-
-  //root.prettyPrintTo(Serial);
-   delay(500);
-  // This prints:
-  // {
-  //   "sensor": "gps",
-  //   "time": 1351824120,
-  //   "data": [
-  //     48.756080,
-  //     2.302038
-  //   ]
-  // }
-
-    // put your main code here, to run repeatedly:
-  /*analog_value = analogRead(ANALOG_PIN_1);
-  Serial.println(analog_value);
-  delay(500);*/
+void loop()
+{
+  Blynk.run();
 }
