d07e887d6d0bbc7e84380b975207e4ad79192692
Rhea Hau <rheahau@gmail.com>
2018-10-28 21:55:03 +0100

    JSON aanmaken gelukt en nu JSON naar CSV

--- prog/PulseBreathGSRsensor/src/create_json_rhea.py
+++ prog/PulseBreathGSRsensor/src/create_json_rhea.py
@@ -15,26 +15,35 @@ def read_to_file():
     pass
 
 if __name__ == "__main__":
-    print("STARTING SCRIPT")
+    print("-----------------STARTING SCRIPT")
 
     # Get ESP32 sensor data
     ser = serial.Serial('COM3', 115200, timeout=1)
     ser.flushInput()
     ser_bytes = ser.readline()
+
+    print(ser_bytes == unicode("hello\n")
+
     data_list = []
+
     while ser_bytes:
-        decoded_bytes = str(ser_bytes[0:len(ser_bytes)-2].decode("ascii"))
+        print("-----------------STARTING JSON VULLEN")
+        decoded_bytes = str(ser_bytes[0:len(ser_bytes)-2].decode('ascii').split(','))
         value = json.loads(decoded_bytes)
 
+        print("-----------------STARTING GETTING TIJD")
         # Get current time
         current_time = datetime.datetime.now()
         date_now = current_time.strftime('%d-%m-%Y')
         time_now = current_time.strftime('%H:%M:%S')
 
+        print("-----------------STARTING JSON TIJD")
         data = {
                 'date': date_now,
                 'time': time_now, 
                 'value':value
             }
+        
+        print("-----------------STARTING JSON DATA PLAKKEN")
         data_list.append(data)
         ser_bytes = ser.readline()
